293ff48bdab2ff57b81c5cadd9b770e034117d48
Maxoumen <maxime.feuillas05@gmail.com>
2025-05-31 21:22:04 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,10 +3,10 @@ const firebaseConfig = {
   apiKey: "AIzaSyAzL3AyyR8Vz9J2Trw_u4Kpo8gXrqtlLLQ",
   authDomain: "listingrep.firebaseapp.com",
   projectId: "listingrep",
-  storageBucket: "listingrep.appspot.com",
-  messagingSenderId: "XXXXXXXX",
-  appId: "1:XXXXXXXX:web:XXXXXXXX"
-};
+  storageBucket: "listingrep.firebasestorage.app",
+  messagingSenderId: "415040498064",
+  appId: "1:415040498064:web:aee5a0e3abfffe84786b15",
+  measurementId: "G-D56JRGQMCW"
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
